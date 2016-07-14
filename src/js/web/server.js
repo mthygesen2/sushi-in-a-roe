c8311d7c4cd7695d5e84434519cc5fe380e1faf4
Mao Thygesen <mao@mash.is>
2016-07-14 16:43:06 -0700

    direct js to correct paths

--- src/js/web/server.js
+++ src/js/web/server.js
@@ -1,15 +1,18 @@
 var express = require('express'),
-    logger = require('morgan'),
-    app = express(),
-    jade = require('jade');
-
+  logger = require('morgan'),
+  app = express(),
+  jade = require('jade');
+var basePath = __dirname + '/../../..'
 app.use(logger('dev'));
-app.use('/', express.static(__dirname + '/static'));
+app.use('/', express.static(basePath));
 
 // 404 handler
 app.use(function(req, res, next) {
   try {
-    var html = jade.compileFile(__dirname + '/src/templates/404.html.jade')({ queryString: req.query });
+    var html = jade.compileFile(basePath + '/src/templates/404.html.jade')
+      ({
+        queryString: req.query
+      });
     res.status(404).send(html)
   } catch (e) {
     next(e)
@@ -17,6 +20,6 @@ app.use(function(req, res, next) {
 });
 
 // Local server
-app.listen(process.env.PORT || 3040, function () {
+app.listen(process.env.PORT || 3040, function() {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3040))
 });
